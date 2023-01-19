e3993f469417372ebd51332681414da27b10280e
polinatea <polinachainikova99@gmail.com>
2023-01-19 13:24:52 +0300

    конечная версия виджета агрегат
    нужно доделать: 1) динамическую смену картинок и их цвета, 2) позиционировать знак warning, 3) цвет рамки

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <script setup>
-import Crab from './components/Crab.vue'
-import Agregat from './components/Agregat.vue'
+  import Crab from './components/Crab.vue';
+  import Agregat from './components/Agregat.vue';
 </script>
 
-<template >
-
-    <div class="bg-[#272933] w-1920px h-screen">
-      <!--vue props--> 
-      <!-- <Crab /> -->
-      <Agregat />
-      
-    </div>
-
-
+<template>
+  <div class="bg-[#272933] w-1920px h-screen">
+    <!--vue props-->
+    <!-- <Crab /> -->
+    <Agregat />
+  </div>
 </template>
 
-<style scoped>
-
+<style>
+  * {
+    box-sizing: border-box;
+  }
 </style>
--- src/components/Agregat.vue
+++ src/components/Agregat.vue
@@ -1,54 +1,141 @@
 <script setup>
+  import { ref } from 'vue';
 
+  const data = ref({
+    name: 'ГД1',
+    state: 'warning',
+    type: 'engine', // ['diesel-generator', 'boiler' ] (дизель-генератор, котел)
+    frequency_value: 216,
+    frequency_type: 'об/мин',
+    params: [
+      {
+        name: 'Мгн. расход',
+        value: '324444',
+        type: 'кг/чxxxx',
+      },
+      {
+        name: 'Мгн. расход',
+        value: '324324',
+        type: 'кг/ч',
+      },
+      {
+        name: 'Мгн. расход',
+        value: '324324',
+        type: 'кг/ч',
+      },
+      {
+        name: 'Мгн. расход',
+        value: '324324',
+        type: 'кг/ч',
+      },
+      {
+        name: 'Мгн. расход',
+        value: '324324',
+        type: 'кг/ч',
+      },
+    ],
+  });
+
+  // Цвета параметров
+  const states = ref({
+    warning: '#FFCC81',
+    error: '#DB8181',
+    default: '#48A7FF',
+    working: '#66FE87',
+  });
+
+  // Изменение цвета состояния по первому параметру (левая шкала, нумерация шкалы и первое значение)
+  function changeStateColor(state) {
+    if (states.value[state]) {
+      // if not undefined
+      return states.value[state];
+    }
+    return states.value.default;
+
+    // if (data.value.type == 'engine' || data.value.type == 'diesel-generator') {
+    //   if (data.value.frequency_value != 0) {
+    //     if (data.value.state == 'warning') {
+    //       stateColor.value = '#FFCC81';
+    //     } else if (data.value.state == 'error') {
+    //       stateColor.value = '#DB8181';
+    //     }
+    //   } else {
+    //     stateColor.value = '#48A7FF';
+    //   }
+    // } else {
+    //   if (data.value.state == 'warning') {
+    //     stateColor.value = '#FFCC81';
+    //   } else if (data.value.state == 'error') {
+    //     stateColor.value = '#DB8181';
+    //   }
+    // }
+    // return stateColor.value;
+  }
 </script>
 
 <template>
-     <div class="w-[13vw] h-[17vh] opacity-40 bg-gradient-to-b p-[0.1vw] from-[#D6EBFF] to-[#272933] rounded-[0.6vw] font-euclid">
-        <div class="bg-[#3E404C] h-full rounded-[0.6vw] flex">
-
-            <div class=" flex flex-col ">
-               <div class="flex flex-row gap-[0.42vw] w-[3.125vw] h-[2.037vh] mt-[1.94vh] ml-[1.09vw] items-center">
-                  <div class="flex items-center w-[1.40625vw] h-[2.037vh] text-[0.9375vw] text-[#66FE87] truncate ">216</div>
-                  <div class="flex items-center w-[1.04vw] h-[2.037vh] text-[0.5208vw] text-[#D6EBFF]  leading-[0.8vh]  break-all">об/мин</div>
-               </div>
-               <div class="mt-[1.9vh] ml-[0.91vw]">
-                  <img src="../assets/icons/agregat.svg" />
-               </div>
-            </div>
-
-
-            <div class="flex flex-col">
-
-               <div class="w-[8.1vw] h-[2.9vh] mt-[1.48vh] ml-[0.50vw] bg-[#272933] rounded-tl-3xl rounded-bl-3xl rounded-tr-0 rounded-br-0 flex items-center">
-                  <div class="ml-[0.83vw] text-[#D6EBFF] font-medium text-[0.94vw] truncate font-euclid"> Гд 1 </div>
-               </div>
-
-               <div class="flex flex-col ml-[0.47vw] mt-[1.46vh]">
-                  <div class="flex flex-row h-[1.4vh] gap-[0.52vw]">
-                     <div class="flex items-center justify-end w-[3.96vw] text-[0.625vw] text-[#D6EBFF]"><div class="truncate">Мгн. расходllll</div>:</div><div class="flex items-center w-[2.86vw] text-[0.73vw] text-[#66FE87] truncate">9 кг/чxxxxxxxxxxx</div>
-                  </div>
-                  <div class="flex flex-row h-[1.4vh] gap-[0.52vw]">
-                     <div class="flex items-center justify-end w-[3.96vw] text-[0.625vw] text-[#D6EBFF]"><div class="truncate">Сут. расход</div>:</div><div class="flex items-center w-[2.86vw] text-[0.73vw] text-[#66FE87] truncate">9 кг/ч</div>
-                  </div>
-                  <div class="flex flex-row h-[1.4vh] gap-[0.52vw]">
-                     <div class="flex items-center justify-end w-[3.96vw] text-[0.625vw] text-[#D6EBFF]"><div class="truncate">Мгн. расходlll</div>:</div><div class="flex items-center w-[2.86vw] text-[0.73vw] text-[#66FE87] truncate">9 кг/ч</div>
-                  </div>
-                  <div class="flex flex-row h-[1.4vh] gap-[0.52vw]">
-                     <div class="flex items-center justify-end w-[3.96vw] text-[0.625vw] text-[#D6EBFF]"><div class="truncate">Мгн. расход</div>:</div><div class="flex items-center w-[2.86vw] text-[0.73vw] text-[#66FE87] truncate">9 кг/ч</div>
-                  </div>
-                  <div class="flex flex-row h-[1.4vh] gap-[0.52vw]">
-                     <div class="flex items-center justify-end w-[3.96vw] text-[0.625vw] text-[#D6EBFF]"><div class="truncate">Мгн. расход</div>:</div><div class="flex items-center w-[2.86vw] text-[0.73vw] text-[#66FE87] truncate">9 кг/ч</div>
-                  </div>
-               </div>
-
-               <div class="flex flex-row mt-[1.2vh] ml-[6vw] gap-[0.2vw] items-center">
-                  <div class="w-[0.68vw] h-[0.93vh] text-[#35D1C8] font-medium text-[0.42vw] font-euclid text-center truncate overflow-hidden flex items-center">WB</div>
-                  <img src="../assets/icons/vector.svg" class=""/>
-                  <div class="w-[0.68vw] h-[0.93vh] text-[#33C300] font-medium text-[0.42vw] font-euclid text-center truncate overflow-hidden flex items-center">ON</div>
-               </div>
-
-            </div>
-
+  <div
+    class="h-[17vh] w-[13vw] rounded-[0.6vw] bg-gradient-to-b from-[#D6EBFF] to-[#272933] p-[0.1vw] font-euclid">
+    <div class="flex h-full rounded-[0.6vw] bg-[#3E404C]">
+      <div class="flex flex-col">
+        <div
+          class="mt-[1.94vh] ml-[1.09vw] flex h-[2.037vh] w-[3.125vw] flex-row items-center gap-[0.42vw]">
+          <div
+            :style="{ color: changeStateColor() }"
+            class="flex h-[2.037vh] w-[1.40625vw] items-center justify-end truncate text-[0.9375vw] text-[#66FE87]">
+            {{ data.frequency_value }}
+          </div>
+          <div
+            class="flex h-[2.037vh] w-[1.04vw] items-center overflow-y-hidden break-all text-[0.5208vw] leading-[0.75vh] text-[#D6EBFF]">
+            {{ data.frequency_type }}
+          </div>
         </div>
-     </div> 
+        <div class="relative mt-[1.9vh] ml-[0.9375vw]">
+          <img class="h-[2.22vh] w-[2.708vw]" src="../assets/icons/agregat/engine.svg" />
+          <img
+            v-if="['error', 'warning'].includes(data.state)"
+            class="absolute left-[0.57vw] top-[0.28vh]"
+            src="../assets/icons/agregat/warning_sign.svg" />
+        </div>
+      </div>
+
+      <div class="flex flex-col">
+        <div
+          class="rounded-tr-0 rounded-br-0 mt-[1.48vh] ml-[0.50vw] flex h-[2.9vh] w-[8.1vw] items-center rounded-tl-3xl rounded-bl-3xl bg-[#272933]">
+          <div class="ml-[0.83vw] truncate font-euclid text-[0.94vw] font-medium text-[#D6EBFF]">
+            {{ data.name }}
+          </div>
+        </div>
+
+        <div class="ml-[0.47vw] mt-[1.46vh] flex flex-col">
+          <div v-for="item in data.params" class="flex h-[1.4vh] flex-row gap-[0.52vw]">
+            <div class="flex w-[3.96vw] items-center justify-end text-[0.625vw] text-[#D6EBFF]">
+              <div class="overflow-hidden">{{ item.name }}</div>
+              :
+            </div>
+            <div
+              :style="{ color: changeStateColor() }"
+              class="flex w-[2.86vw] items-center gap-[0.15vw] text-[0.73vw] text-[#66FE87]">
+              <div class="overflow-hidden whitespace-nowrap">{{ item.value }}</div>
+              <div class="w-[1.5vw] overflow-hidden whitespace-nowrap">
+                {{ item.type }}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-[1.2vh] ml-[6vw] flex flex-row items-center gap-[0.2vw]">
+          <div
+            class="flex h-[0.93vh] w-[0.68vw] items-center overflow-hidden truncate text-center font-euclid text-[0.42vw] font-medium text-[#35D1C8]">
+            WB
+          </div>
+          <img src="../assets/icons/agregat/vector.svg" class="" />
+          <div
+            class="flex h-[0.93vh] w-[0.68vw] items-center overflow-hidden truncate text-center font-euclid text-[0.42vw] font-medium text-[#33C300]">
+            ON
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
