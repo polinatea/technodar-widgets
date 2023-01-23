2730150a50b4a3adc37b62048d39518d5249789a
polinatea <polinachainikova99@gmail.com>
2023-01-23 12:31:35 +0300

    Обработка движения значков

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
   import Crab from './components/Crab.vue';
   import Agregat from './components/Agregat.vue';
+  import Water_agregat from './components/Water_agregat.vue';
 </script>
 
 <template>
-  <div class="bg-[#272933] w-1920px h-screen">
+  <div class="w-1920px h-screen bg-[#272933]">
     <!--vue props-->
     <!-- <Crab /> -->
-    <Agregat />
+    <!-- <Agregat /> -->
+    <Water_agregat />
   </div>
 </template>
 
--- src/components/Agregat.vue
+++ src/components/Agregat.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref } from 'vue';
+  import { ref, computed } from 'vue';
 
   const data = ref({
     name: 'ГД1',
